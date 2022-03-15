307b4bbcaf453373d84c815b0986e45929bc4870
timf34 <farrelti@tcd.ie>
2022-03-15 13:06:16 +0000

    Added device connection without chat page + TODOs

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import "device_bluetooth.dart";
-import "match_selection.dart";
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
+import "match_selection.dart";
 import "discovery_page.dart";
 import "select_bonded_device_page.dart";
+import "chat_page.dart";
+
+
+// TODO: the device paring and opening straight into the chat page works well
+//  for now, however its not going to work very well for the final device.
+//  Need a cleaner way to simply connect to the device and confirm that its
+//  connected
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
@@ -26,44 +33,67 @@ class SecondPage extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const SelectBondedDevicePage(checkAvailability: false))
+                ElevatedButton(
+                  child: const Text("Connect + serial communication (debugging)"),
+                  onPressed: () async {
+                    final BluetoothDevice? selectedDevice =
+                    await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SelectBondedDevicePage(checkAvailability: false);
+                        },
+                      ),
                     );
+
+                    if (selectedDevice != null) {
+                      print('Connect -> selected ' + selectedDevice.address);
+                      _startChat(context, selectedDevice);
+                    } else {
+                      print('Connect -> no device selected');
+                    }
                   },
-                  child: const Text('Connect to paired bluetooth devices'),
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20),
-                    primary: Colors.white, // This sets the colour of the font
-                    backgroundColor: Colors.blue, // This sets the colour of the button background
-                  ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const DiscoveryPage()),
+                ElevatedButton(
+                  child: const Text("Connect to Bluetooth Devices (no communication)"),
+                  onPressed: () async {
+                    final BluetoothDevice? selectedDevice =
+                    await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SelectBondedDevicePage(checkAvailability: false);
+                          // TODO: note that this currently only displays
+                          // devices that have been paired with already via the
+                          // phones bluetooth settings. discovery_page.dart
+                          // extends this to discovering all devices, we should
+                          // look into ways to do the same, and to actually
+                          // pair devices that haven't been paired before,
+                          // through the app. This will probably involve
+                          // opening the bluetooth settings directly from the
+                          // app.
+                        },
+                      ),
                     );
+
+                    if (selectedDevice != null) {
+                      print('Connect -> selected ' + selectedDevice.address);
+                      // _startChat(context, selectedDevice);
+                      // TODO: a popup confirming the connection
+                    } else {
+                      print('Connect -> no device selected');
+                      // TODO: a popup confirming the connection didn't work
+                    }
                   },
-                  child: const Text('Bluetooth Serial Communication'),
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20),
-                    primary: Colors.white, // This sets the colour of the font
-                    backgroundColor: Colors.blue, // This sets the colour of the button background
-                  ),
                 ),
-                TextButton(
+                ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => const MatchSelection()),
                     );
                   },
-                  child: const Text('Matches page (goes to Bluetooth page for now)'),
+                  child: const Text('Matches page (empty for now)'),
                   style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20),
+                    textStyle: const TextStyle(fontSize: 16),
                     primary: Colors.white, // This sets the colour of the font
                     backgroundColor: Colors.blue, // This sets the colour of the button background
                   ),
@@ -76,3 +106,13 @@ class SecondPage extends StatelessWidget {
         );
     }
   }
+
+void _startChat(BuildContext context, BluetoothDevice server) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) {
+        return ChatPage(server: server);
+      },
+    ),
+  );
+}
